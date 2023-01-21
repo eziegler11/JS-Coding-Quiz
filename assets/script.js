2fe3a507f8048ed6f8f06ddc3fc6c8ae4716f5a2
erichziegler <erich.ziegler11@gmail.com>
2023-01-21 16:55:27 -0700

    Basic HTML file built with initial styling in CSS.

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+// Acceptance Criteria
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+// Landing Page - View Highscores & Timer, CSS: flex, space-between
+// H1 Coding Quiz Challenge
+// P with text
+// Button to start quiz
+// DIV wrapping the above minus "nav bar / header"
+
+// Starting the quiz - timer starts
+// Store quiz questions and answers in an object, plus which is correct answer
+// Each answer is a button
+// Event listener for each answer button
+// When user clicks on an answer button
+// If answer is correct, put "Correct!" below the answer buttons
+// If answer is incorrect, put "Wrong!" below the answer buttons, subtract 10 seconds from the timer
+
+// Once through all questions or timer reaches 0, game is over
+// Once through all questions and timer still has time, the game ends and that is users score
+// Displays users final score based on time left and adds input box to enter initials
+// Submit button to save initials and score to local storage
+// Then displays the highscores page
